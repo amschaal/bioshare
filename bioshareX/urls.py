1c0bc7804ef672c85b7b6f398c507feb7726c6ea
adam <amschaal@ucdavis.edu>
2013-07-08 11:22:49 -0700

    make sshwrapper use REST request for auth

--- bioshareX/urls.py
+++ bioshareX/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('bioshareX.views',
 )
 urlpatterns += patterns('bioshareX.api',
     url(r'^api/get_permissions/(?P<share>\w{15})/?$', 'get_permissions', name='api_get_permissions'),
+    url(r'^api/get_user_permissions/(?P<share>\w{15})/?$', 'get_user_permissions', name='api_get_user_permissions'),
     url(r'^api/set_permissions/(?P<share>\w{15})/?$', 'set_permissions', name='api_set_permissions'),
     url(r'^api/update/(?P<share>\w{15})/?$', 'update_share', name='api_update_share'),
     url(r'^api/get_user/?$', 'get_user', name='api_get_user'),
