84e31bee3088ecc863f2c7740a7d4d1cbdb870bd
adam <amschaal@ucdavis.edu>
2013-07-16 14:54:28 -0700

    add autocomplete, and share list page

--- bioshareX/contexts.py
+++ bioshareX/contexts.py
@@ -2,8 +2,8 @@ from models import Share
 from guardian.shortcuts import get_objects_for_user
 def user_contexts(request):
         if request.user.is_authenticated():
-            recent_shares = Share.objects.filter(owner=request.user)
-            shared_with_me = get_objects_for_user(request.user, 'bioshareX.view_share_files')
+            recent_shares = Share.objects.filter(owner=request.user)[:5]
+            shared_with_me = get_objects_for_user(request.user, 'bioshareX.view_share_files')[:5]
         else:
             recent_shares = []
             shared_with_me = [] 
@@ -14,7 +14,7 @@ def user_contexts(request):
 
 
 
-def user_dashboard(request, template_name='projects/dashboard.html'):
-    projects = get_objects_for_user(request.user, 'projects.view_project')
-    return render_to_response(template_name, {'projects': projects},
-        RequestContext(request))
+# def user_dashboard(request, template_name='projects/dashboard.html'):
+#     projects = get_objects_for_user(request.user, 'projects.view_project')
+#     return render_to_response(template_name, {'projects': projects},
+#         RequestContext(request))
---
+++ static_files/js/bioshare.js
@@ -0,0 +1,21 @@
+function share_autocomplete(query,process){
+	$.get(share_autocomplete_url,{query:query},function(data){
+		if(data.errors)
+			BC.handle_ajax_errors(data);
+		else if(data.shares){
+			var shares = $.map(data.shares,function(share,index){
+				return '<span data-toggle="tooltip" title="'+share.notes+'" data-url="'+share.url+'">'+share.name+'</span>';
+			});
+			process(shares);
+		}
+	});
+}
+function share_autocomplete_select(obj) {
+	var share = $(obj);
+	window.location.href = share.attr('data-url');
+	return share.text();
+    //window.location.href = obj.url;
+}
+$(function () {
+	$('#share_autocomplete').typeahead({source:share_autocomplete,updater:share_autocomplete_select})
+});
