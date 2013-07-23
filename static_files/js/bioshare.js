f0551c9faf5bc6afbfc5fa16c857dec1355bf015
adam <amschaal@ucdavis.edu>
2013-07-23 12:12:13 -0700

    javascript stuff

--- static_files/js/bioshare.js
+++ static_files/js/bioshare.js
@@ -10,12 +10,34 @@ function share_autocomplete(query,process){
 		}
 	});
 }
+function share_autocomplete_match(obj){
+	return true;
+}
 function share_autocomplete_select(obj) {
 	var share = $(obj);
 	window.location.href = share.attr('data-url');
 	return share.text();
     //window.location.href = obj.url;
 }
+function preg_quote( str ) {
+     return (str+'').replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
+}
+
+function highlight( data, search )
+{
+    return data.replace( new RegExp( "(" + preg_quote( search ) + ")" , 'gi' ), "<b>$1</b>" );
+}
+function share_autocomplete_highlighter(item){
+	console.log('highlight',this.query);
+	var text = $(item).text();
+	$.each(this.query.split(' '),function(index,term){
+		if (term.length >1)
+			text = highlight(text,term);
+		//text = text.replace(term,'<b>'+term+'</b>');
+	});
+	return text;
+	return $(item).text(text).clone().wrap('<div>').parent().html();
+}
 $(function () {
-	$('#share_autocomplete').typeahead({source:share_autocomplete,updater:share_autocomplete_select,minLength:2})
+	$('#share_autocomplete').typeahead({highlighter:share_autocomplete_highlighter,source:share_autocomplete,updater:share_autocomplete_select,minLength:2,matcher:share_autocomplete_match})
 });
