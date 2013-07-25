ec0f5349ab8399c26af7dc3fab2c2257505b5e9b
adam <amschaal@ucdavis.edu>
2013-07-25 09:52:45 -0700

    new template tag for linking paths

---
+++ bioshareX/templatetags/share_tags.py
@@ -0,0 +1,18 @@
+from django import template
+# from django.template.Library import register
+register = template.Library()
+from django.core.urlresolvers import reverse
+import os
+@register.simple_tag
+def link_full_path(*args, **kwargs):
+    share = kwargs['share']
+    subpath = kwargs['subpath']
+    paths = subpath.split(os.sep)
+    links = []
+    for ind, val in enumerate(paths):
+        spath = os.sep.join(paths[:ind+1])
+        url = reverse('go_to_file_or_folder',kwargs={'share':share.id,'subpath':spath})
+        link = '<a href="%s">%s</a>'%(url,val)
+        links.append(link)
+#     url = reverse('go_to_file_or_folder',kwargs={'share':share.id,'subpath':subpath})
+    return ' / '.join(links) 
