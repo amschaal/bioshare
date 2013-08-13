758023307687dbb0602e4ca285b3b618e4fd115e
adam <amschaal@ucdavis.edu>
2013-08-13 15:05:02 -0700

    make compatible with older python

--- bioshareX/file_utils.py
+++ bioshareX/file_utils.py
@@ -44,5 +44,5 @@ def get_lines(path,from_line=1,to_line=100):
     return subprocess.Popen(['sed', '-n','%d,%dp; %dq' % (from_line,to_line,to_line+1), path], stdout=subprocess.PIPE).stdout.read()
 #     sed '5555,7777p; 7778q' filename
 def get_num_lines(path):
-    return int(subprocess.check_output(['wc', '-l', path]).split()[0])
+    return int(subprocess.Popen(['wc', '-l', path], stdout=subprocess.PIPE).stdout.read().split()[0])
 
