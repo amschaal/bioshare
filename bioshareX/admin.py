e81f49ea955328b6c220d5119194ac6d48620e65
adam <amschaal@ucdavis.edu>
2013-10-04 17:01:28 -0700

    adding multiple filesystem functionality

--- bioshareX/admin.py
+++ bioshareX/admin.py
@@ -1,10 +1,36 @@
 from django.contrib import admin
-from bioshareX.models import Share
+from bioshareX.models import Share, Filesystem
 from guardian.admin import GuardedModelAdmin
+from django.contrib.auth.admin import UserAdmin as OriginalUserAdmin
+from django.contrib.auth.models import User
 class ShareAdmin(GuardedModelAdmin):
     pass
 #     excl = ('name', 'notes')
 #     def save_model(self, request, obj, form, change):
 #         obj.owner = request.user
 #         obj.save()
+class FilesystemAdmin(GuardedModelAdmin):
+    pass
+
+class FSInline(admin.TabularInline):
+    """ As you are noticed your profile will be edited as inline form """
+    model = Filesystem.users.through
+#     fk_name = ""
+
+class UserAdmin(OriginalUserAdmin):
+    """ Just add inlines to the original UserAdmin class """
+    inlines = [FSInline, ]
+#     def formfield_for_manytomany(self, db_field, request, **kwargs):
+#         if db_field.name == "cars":
+#             kwargs["queryset"] = Car.objects.filter(owner=request.user)
+#         return super(MyModelAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
+ 
+try:
+    admin.site.unregister(User)
+finally:
+    admin.site.register(User, UserAdmin)
+
+
 admin.site.register(Share, ShareAdmin)
+admin.site.register(Filesystem, FilesystemAdmin)
+
