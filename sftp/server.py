83a5051b13dfe2bb7406eb153e8e1ab98db222d2
Adam Schaal <amschaal@ucdavis.edu>
2017-01-18 17:34:04 -0800

    Adding more security to SFTP server using Bioshare permissions

--- sftp/server.py
+++ sftp/server.py
@@ -6,7 +6,7 @@ import socket
 import threading
 from django.contrib.auth import authenticate
 import paramiko
-from paramiko.sftp import SFTP_OK
+from paramiko.sftp import SFTP_OK, SFTP_OP_UNSUPPORTED
 from paramiko.common import o666
 from bioshareX.models import Share
 
@@ -87,7 +87,6 @@ class SSHInterface(paramiko.ServerInterface):
 class PermissionDenied(Exception):
     pass
 
-
 def sftp_response(fn):
     """
     Dectorator which converts exceptions into appropriate SFTP error codes,
@@ -108,6 +107,25 @@ def sftp_response(fn):
     return wrapper
 
 
+class permissions_required(object):
+
+    def __init__(self, permissions=[],path_arg_ind=1,operator='AND'):
+        self.permissions = permissions
+        self.path_arg_index = path_arg_ind
+        self.operator = operator
+    def __call__(self, f):
+        def wrapped_f(*args,**kwargs):
+            interface = args[0]
+            path = args[self.path_arg_index]
+            if path == '/':
+                return f(*args,**kwargs)
+            permissions = interface._get_bioshare_path_permissions(path)
+            perm_num = len(set(permissions).intersection(set(self.permissions)))
+            if self.operator == 'OR' and perm_num == 0 or self.operator == 'AND' and len(self.permissions) != perm_num: 
+                raise PermissionDenied()
+            return f(*args,**kwargs)
+        return wrapped_f
+
 def log_event(method):
     """
     Decorator which logs SFTP events along with the current user
@@ -127,7 +145,7 @@ def log_event(method):
     return wrapper
 
 
-class StubSFTPHandle (paramiko.SFTPHandle):
+class BioshareSFTPHandle (paramiko.SFTPHandle):
     def stat(self):
         try:
             return paramiko.SFTPAttributes.from_stat(os.fstat(self.readfile.fileno()))
@@ -166,9 +184,7 @@ class SFTPInterface (paramiko.SFTPServerInterface):
         print 'user'
         print self.user
 #         self.ROOT = root
-    def _realpath(self, path):
-        print 'realpath'
-        print path
+    def _get_share(self,path):
         parts = path.split(os.path.sep)
         print parts
         if len(parts) < 2:
@@ -177,11 +193,32 @@ class SFTPInterface (paramiko.SFTPServerInterface):
         if not self.shares.has_key(parts[1]):
             print 'no share exists'
             raise PermissionDenied("Share does not exist: %s"%path[1])
-        share = self.shares[parts[1]]
+        return self.shares[parts[1]]
+    def _get_bioshare_path_permissions(self,path):
+        share = self._get_share(path)
+        permissions = share.get_user_permissions(self.user)
+        print permissions
+        return permissions
+    def _get_path_permissions(self,path):
+        permissions = self._get_bioshare_path_permissions(path)
+        #translate
+#         PERMISSION_VIEW = 'view_share_files'
+#         PERMISSION_DELETE = 'delete_share_files'
+#         PERMISSION_DOWNLOAD = 'download_share_files'
+#         PERMISSION_WRITE = 'write_to_share'
+#         PERMISSION_LINK_TO_PATH = 'link_to_path'
+#         PERMISSION_ADMIN
+        return permissions
+    def _realpath(self, path):
+        print 'realpath'
+        print path
+        parts = path.split(os.path.sep)
+        share = self._get_share(path)
         realpath = os.path.join(share.get_realpath(),os.path.sep.join(parts[2:]))
         return realpath
 #         print self.ROOT + self.canonicalize(path)
 #         return self.ROOT + self.canonicalize(path)
+    @sftp_response
     def list_shares(self):
         print "LIST SHARES"
         try:
@@ -197,9 +234,12 @@ class SFTPInterface (paramiko.SFTPServerInterface):
             return out
         except OSError as e:
             return SFTPServer.convert_errno(e.errno)
+    @sftp_response
+    @permissions_required([Share.PERMISSION_ADMIN])
     def list_folder(self, path):
         if path == '/':
             return self.list_shares()
+        permissions = self._get_path_permissions(path)
         path = self._realpath(path)
         try:
             out = []
@@ -213,20 +253,23 @@ class SFTPInterface (paramiko.SFTPServerInterface):
         except OSError as e:
             return SFTPServer.convert_errno(e.errno)
 
+    @sftp_response
+    @permissions_required([Share.PERMISSION_VIEW])
     def stat(self, path):
         path = self._realpath(path)
         try:
             return paramiko.SFTPAttributes.from_stat(os.stat(path))
         except OSError as e:
             return SFTPServer.convert_errno(e.errno)
-
+    @sftp_response
+    @permissions_required([Share.PERMISSION_VIEW])
     def lstat(self, path):
         path = self._realpath(path)
         try:
             return paramiko.SFTPAttributes.from_stat(os.lstat(path))
         except OSError as e:
             return SFTPServer.convert_errno(e.errno)
-
+    @sftp_response
     def open(self, path, flags, attr):
         path = self._realpath(path)
         try:
@@ -261,20 +304,24 @@ class SFTPInterface (paramiko.SFTPServerInterface):
             f = os.fdopen(fd, fstr)
         except OSError as e:
             return SFTPServer.convert_errno(e.errno)
-        fobj = StubSFTPHandle(flags)
+        fobj = BioshareSFTPHandle(flags)
         fobj.filename = path
         fobj.readfile = f
         fobj.writefile = f
         return fobj
-
+    @sftp_response
+    @permissions_required([Share.PERMISSION_DELETE])
     def remove(self, path):
+#         if not Share.PERMISSION_DELETE in self._get_bioshare_path_permissions(path):
+#             return PermissionDenied()
         path = self._realpath(path)
         try:
             os.remove(path)
         except OSError as e:
             return SFTPServer.convert_errno(e.errno)
         return SFTP_OK
-
+    @sftp_response
+    @permissions_required([Share.PERMISSION_DELETE,Share.PERMISSION_WRITE])
     def rename(self, oldpath, newpath):
         oldpath = self._realpath(oldpath)
         newpath = self._realpath(newpath)
@@ -283,7 +330,8 @@ class SFTPInterface (paramiko.SFTPServerInterface):
         except OSError as e:
             return SFTPServer.convert_errno(e.errno)
         return SFTP_OK
-
+    @sftp_response
+    @permissions_required([Share.PERMISSION_WRITE])
     def mkdir(self, path, attr):
         path = self._realpath(path)
         try:
@@ -293,7 +341,8 @@ class SFTPInterface (paramiko.SFTPServerInterface):
         except OSError as e:
             return SFTPServer.convert_errno(e.errno)
         return SFTP_OK
-
+    @sftp_response
+    @permissions_required([Share.PERMISSION_DELETE])
     def rmdir(self, path):
         path = self._realpath(path)
         try:
@@ -301,172 +350,54 @@ class SFTPInterface (paramiko.SFTPServerInterface):
         except OSError as e:
             return SFTPServer.convert_errno(e.errno)
         return SFTP_OK
-
+    @sftp_response
     def chattr(self, path, attr):
-        path = self._realpath(path)
-        try:
-            SFTPServer.set_file_attr(path, attr)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        return SFTP_OK
-
+        return SFTP_OP_UNSUPPORTED
+#         path = self._realpath(path)
+#         try:
+#             SFTPServer.set_file_attr(path, attr)
+#         except OSError as e:
+#             return SFTPServer.convert_errno(e.errno)
+#         return SFTP_OK
+    @sftp_response
     def symlink(self, target_path, path):
-        path = self._realpath(path)
-        if (len(target_path) > 0) and (target_path[0] == '/'):
-            # absolute symlink
-            target_path = os.path.join(self.ROOT, target_path[1:])
-            if target_path[:2] == '//':
-                # bug in os.path.join
-                target_path = target_path[1:]
-        else:
-            # compute relative to path
-            abspath = os.path.join(os.path.dirname(path), target_path)
-            if abspath[:len(self.ROOT)] != self.ROOT:
-                # this symlink isn't going to work anyway -- just break it immediately
-                target_path = '<error>'
-        try:
-            os.symlink(target_path, path)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        return SFTP_OK
-
+        return SFTP_OP_UNSUPPORTED
+#         path = self._realpath(path)
+#         if (len(target_path) > 0) and (target_path[0] == '/'):
+#             # absolute symlink
+#             target_path = os.path.join(self.ROOT, target_path[1:])
+#             if target_path[:2] == '//':
+#                 # bug in os.path.join
+#                 target_path = target_path[1:]
+#         else:
+#             # compute relative to path
+#             abspath = os.path.join(os.path.dirname(path), target_path)
+#             if abspath[:len(self.ROOT)] != self.ROOT:
+#                 # this symlink isn't going to work anyway -- just break it immediately
+#                 target_path = '<error>'
+#         try:
+#             os.symlink(target_path, path)
+#         except OSError as e:
+#             return SFTPServer.convert_errno(e.errno)
+#         return SFTP_OK
+    @sftp_response
     def readlink(self, path):
-        path = self._realpath(path)
-        try:
-            symlink = os.readlink(path)
-        except OSError as e:
-            return SFTPServer.convert_errno(e.errno)
-        # if it's absolute, remove the root
-        if os.path.isabs(symlink):
-            if symlink[:len(self.ROOT)] == self.ROOT:
-                symlink = symlink[len(self.ROOT):]
-                if (len(symlink) == 0) or (symlink[0] != '/'):
-                    symlink = '/' + symlink
-            else:
-                symlink = '<error>'
-        return symlink
-"""
-class SFTPInterfaceExample(paramiko.SFTPServerInterface):
+        return SFTP_OP_UNSUPPORTED
+#         path = self._realpath(path)
+#         try:
+#             symlink = os.readlink(path)
+#         except OSError as e:
+#             return SFTPServer.convert_errno(e.errno)
+#         # if it's absolute, remove the root
+#         if os.path.isabs(symlink):
+#             if symlink[:len(self.ROOT)] == self.ROOT:
+#                 symlink = symlink[len(self.ROOT):]
+#                 if (len(symlink) == 0) or (symlink[0] != '/'):
+#                     symlink = '/' + symlink
+#             else:
+#                 symlink = '<error>'
+#         return symlink
 
-    FILE_MODE = 0664
-    DIRECTORY_MODE = 0775
-
-    def __init__(self, server, root):
-        self.user = server.user
-        self.root = root
-
-    def realpath_for_read(self, path):
-        return self._realpath(path, self.user.has_read_access)
-
-    def realpath_for_write(self, path):
-        return self._realpath(path, self.user.has_write_access)
-
-    def _realpath(self, path, permission_check):
-        path = self.canonicalize(path).lstrip('/')
-        if not permission_check(path):
-            raise PermissionDenied()
-        return os.path.join(self.root, path)
-
-    @sftp_response
-    @log_event
-    def open(self, path, flags, attr):
-        # We ignore `attr` -- we choose the permissions around here,
-        # not the client
-        read_only = (flags == os.O_RDONLY)
-        if read_only:
-            realpath = self.realpath_for_read(path)
-        else:
-            realpath = self.realpath_for_write(path)
-        fd = os.open(realpath, flags, self.FILE_MODE)
-        fileobj = os.fdopen(fd, flags_to_string(flags), self.FILE_MODE)
-        handle = SFTPFileHandle(flags)
-        handle.readfile = fileobj
-        if not read_only:
-            handle.writefile = fileobj
-        return handle
-
-    @sftp_response
-    @log_event
-    def list_folder(self, path):
-        realpath = self.realpath_for_read(path)
-        return [sftp_attributes(os.path.join(realpath, filename))
-                    for filename in os.listdir(realpath)]
-
-    @sftp_response
-    @log_event
-    def stat(self, path):
-        return sftp_attributes(self.realpath_for_read(path), follow_links=True)
-
-    @sftp_response
-    def lstat(self, path):
-        return sftp_attributes(self.realpath_for_read(path))
-
-    @sftp_response
-    @log_event
-    def remove(self, path):
-        os.unlink(self.realpath_for_write(path))
-
-    @sftp_response
-    @log_event
-    def rename(self, oldpath, newpath):
-        realpath_old = self.realpath_for_write(oldpath)
-        realpath_new = self.realpath_for_write(newpath)
-        # SFTP dictates that renames should be non-destructive
-        # (Yes, there's a race-condition here, but we can live
-        # with it)
-        if os.path.exists(realpath_new):
-            raise OSError(errno.EEXIST)
-        os.rename(realpath_old, realpath_new)
-
-    @sftp_response
-    @log_event
-    def mkdir(self, path, attr):
-        # We ignore `attr` -- we choose the permissions around here,
-        # not the client
-        os.mkdir(self.realpath_for_write(path), self.DIRECTORY_MODE)
-
-    @sftp_response
-    @log_event
-    def rmdir(self, path):
-        os.rmdir(self.realpath_for_write(path))
-
-    @sftp_response
-    @log_event
-    def chattr(self, path, attr):
-        # We flat-out lie and pretend that we've executed this
-        # but don't actually do anything
-        pass
-
-    @sftp_response
-    @log_event
-    def readlink(self, path):
-        # We only allow `readlink` on relative links that stay within the
-        # shared root directory
-        realpath = self.realpath_for_read(path)
-        target = os.readlink(realpath)
-        if os.path.isabs(target):
-            return paramiko.SFTP_OP_UNSUPPORTED
-        target_abs = os.path.normpath(os.path.join(
-            os.path.dirname(realpath), target))
-        if not target_abs.startswith(self.root + '/'):
-            return paramiko.SFTP_OP_UNSUPPORTED
-        return target
-"""
-"""
-class SFTPFileHandle(paramiko.SFTPHandle):
-
-    @sftp_response
-    def chattr(self, path, attr):
-        # We flat-out lie and pretend that we've executed this
-        # but don't actually do anything
-        pass
-
-    @sftp_response
-    def stat(self):
-        return paramiko.SFTPAttributes.from_stat(
-                os.fstat(self.readfile.fileno()))
-
-"""
 def sftp_attributes(filepath, follow_links=False):
     """
     Return an SFTPAttributes object for the given path
