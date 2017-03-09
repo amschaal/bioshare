256f719d20e40671f84a02d6f8613eab5abbe465
Adam Schaal <amschaal@ucdavis.edu>
2017-03-09 10:43:04 -0800

    Supress annoying DisallowedHost security exceptions from being sent by email

--- settings/settings.py
+++ settings/settings.py
@@ -149,11 +149,6 @@ INSTALLED_APPS = (
     'compressor',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -167,7 +162,11 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
     },
     'loggers': {
         'django.request': {
@@ -175,9 +174,12 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
+    'django.security.DisallowedHost': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
     }
 }
-
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend', # this is default
