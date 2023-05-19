38a46b64f6313c42083d07064f146ec83aa0da35
Adam Schaal <amschaal@gmail.com>
2023-05-19 14:34:12 -0700

    Remove site wide file search.  Show if share is locked on share search page.

--- bioshareX/urls.py
+++ bioshareX/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
 #     url(r'^account/update_password/?$', 'update_password', name='update_password'),
     url(r'^delete_share/(?P<share>[\da-zA-Z]{15})/?$', bioshare_views.delete_share, kwargs={'confirm':False},name='delete_share'),
     url(r'^confirm_delete_share/(?P<share>[\da-zA-Z]{15})/?$', bioshare_views.delete_share, kwargs={'confirm':True},name='confirm_delete_share'),
-    url(r'^search/files/?$', bioshare_views.search_files, name='search_files'),
+    # url(r'^search/files/?$', bioshare_views.search_files, name='search_files'),
     url(r'^locked/(?P<share>[-\w]+)/$', bioshare_views.locked, name='locked'),
     url(r'^unlock/(?P<share>[-\w]+)/$', bioshare_views.unlock, name='unlock')
     # url(r'^jsurls.js$', jsutils.jsurls, {}, 'jsurls'), # @todo: replace this
